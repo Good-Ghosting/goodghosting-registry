942f7cd271f50c4c8d232a90d9c01f0e9978d742
viraj124 <virajm72@gmail.com>
2022-07-28 16:37:12 +0530

    feat: add registry contract

---
+++ contracts/Registry.sol
@@ -0,0 +1,79 @@
+pragma solidity 0.8.7;
+
+import "@openzeppelin/contracts/access/AccessControl.sol";
+
+// custom errors
+error INVALID_POOL();
+error NOT_AUTHORIZED();
+
+/**
+@notice Registry Contract to keep a track of all goodghosting contracts.
+*/
+contract Registry is AccessControl {
+    event LOG_NEW_POOL(address indexed caller, address indexed pool);
+
+    bytes32 public constant ADMIN_ROLE = keccak256("ADMIN_ROLE");
+    bytes32 public constant OPERATOR_ROLE = keccak256("OPERATOR_ROLE");
+
+    // mapping with pool address as the key
+    mapping(address => bool) public pools;
+
+    /**
+    @dev checks if a pool address is valid
+    @param _pool pool contract address
+    */
+    function isValid(address _pool) internal view {
+        uint32 size;
+        assembly {
+            size := extcodesize(_pool)
+        }
+        if (size == 0) {
+            revert INVALID_POOL();
+        }
+    }
+
+    /**
+    @dev Constructor granting admin role
+    */
+    constructor() {
+        _setRoleAdmin(ADMIN_ROLE, ADMIN_ROLE);
+        _grantRole(ADMIN_ROLE, msg.sender);
+    }
+
+    /**
+    @dev Add pools in the registry
+    @param _pools addresses of the pools
+    */
+    function addContract(address[] memory _pools) external {
+        if (hasRole(ADMIN_ROLE, msg.sender) || hasRole(OPERATOR_ROLE, msg.sender)) {
+            for (uint256 i = 0; i < _pools.length; ) {
+                isValid(_pools[i]);
+                pools[_pools[i]] = true;
+                emit LOG_NEW_POOL(msg.sender, _pools[i]);
+                unchecked {
+                    ++i;
+                }
+            }
+        } else {
+            revert NOT_AUTHORIZED();
+        }
+    }
+
+    /**
+    @dev Grant a operator role
+    @param _operator addresses of the operator
+    */
+    function addOperator(address _operator) external {
+        // can only be granted by the admin
+        grantRole(OPERATOR_ROLE, _operator);
+    }
+
+    /**
+    @dev Revoke a operator role
+    @param _operator addresses of the operator
+    */
+    function removeOperator(address _operator) external {
+        // can only be revoked by the admin
+        revokeRole(OPERATOR_ROLE, _operator);
+    }
+}
