9e2578325b67f493583d5fbdef2fe70bb67ebf36
fodisi <francis2301@gmail.com>
2022-08-05 22:20:04 -0400

    feat: improve registry.sol and registry.test.ts

--- test/registry.test.ts
+++ test/registry.test.ts
@@ -56,6 +56,26 @@ describe("Registry Tests", () => {
     await expect(result).to.emit(registryInstance, "LOG_NEW_POOL").withArgs(user.address, mockPool.address);
   });
 
+    it("updates pool index when adding new contract to the registry", async () => {
+    const [deployer] = await ethers.getSigners();
+    const mockDeployer = new MockPool__factory(deployer);
+    const newPool = await mockDeployer.deploy();
+    await registryInstance.addContract([mockPool.address, newPool.address]);
+    const mockIndex = await registryInstance.poolIndex(mockPool.address);
+    expect(mockIndex).equal(ethers.BigNumber.from('0'), 'mock pool index not set');
+    const newPoolIndex = await registryInstance.poolIndex(newPool.address);
+    expect(newPoolIndex).equal(ethers.BigNumber.from('1'), 'new pool index not set');
+  });
+
+  it("emits multiple events when adding multiple contracts to the registry", async () => {
+    const [deployer] = await ethers.getSigners();
+    const mockDeployer = new MockPool__factory(deployer);
+    const newPool = await mockDeployer.deploy();
+    const result = await registryInstance.addContract([mockPool.address, newPool.address]);
+    await expect(result).to.emit(registryInstance, "LOG_NEW_POOL").withArgs(deployer.address, mockPool.address);
+    await expect(result).to.emit(registryInstance, "LOG_NEW_POOL").withArgs(deployer.address, newPool.address);
+  });
+
   it("reverts if a random user tries to remove a contract from the registry", async () => {
     const [, user] = await ethers.getSigners();
     await expect(registryInstance.connect(user).removeContract(mockPool.address)).to.be.revertedWith(
@@ -63,7 +83,16 @@ describe("Registry Tests", () => {
     );
   });
 
-  it("reverts if the admin tries to remove the a non-existant contract from the registry", async () => {
+  it("reverts if operator tries to remove a contract from the registry", async () => {
+    const [, operator] = await ethers.getSigners();
+    const operatorRole = await registryInstance.OPERATOR_ROLE();
+    await registryInstance.grantRole(operatorRole, operator.address);
+    await expect(registryInstance.connect(operator).removeContract(mockPool.address)).to.be.revertedWith(
+      "NOT_AUTHORIZED()",
+    );
+  });
+
+  it("reverts if the admin tries to remove the a non-existent contract from the registry", async () => {
     await expect(registryInstance.removeContract(mockPool.address)).to.be.revertedWith("POOL_DOES_NOT_EXIST()");
   });
 
@@ -73,10 +102,16 @@ describe("Registry Tests", () => {
     const mockDeployer = new MockPool__factory(deployer);
     const newPool = await mockDeployer.deploy();
     await registryInstance.addContract([mockPool.address, newPool.address]);
-    await registryInstance.removeContract(newPool.address);
+    // ensures new pool index is properly set
+    const originalIndex = await registryInstance.poolIndex(newPool.address);
+    expect(originalIndex.toString()).equal('1', 'index not reset');
+    const result = await registryInstance.removeContract(newPool.address);
+    await expect(result).to.emit(registryInstance, "LOG_POOL_REMOVED").withArgs(deployer.address, newPool.address);
     const status = await registryInstance.poolStatus(newPool.address);
-    assert(!status);
+    expect(status).equal(false, 'status not reset to false');
     const removedPoolIndexValue = await registryInstance.pools(1);
-    assert(removedPoolIndexValue == ZERO_ADDRESS);
+    expect(removedPoolIndexValue).equal(ZERO_ADDRESS, 'address not reset to 0x');
+    const indexReset = await registryInstance.poolIndex(newPool.address);
+    expect(indexReset.toString()).equal('0', 'index not reset');
   });
 });
